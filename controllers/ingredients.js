9fdf53a7044595d22c6493ba8939fb4441066fb7
Matteus Gaarder <matteus.gaarder@gmail.com>
2023-09-08 20:50:08 +0100

    commit before bed

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -29,6 +29,10 @@ const addIngredients = asyncWrapper(async (req, res, next) => {
     const ingredients = req.body.ingredients;
     console.log(ingredients);
     const ingredientObjects = ingredients.map(name => ({ name }));
+    // name => { // can also be written as 
+    //     return { name: name };
+    //   }
+    console.log(ingredientObjects)
     const insertedIngredients = await Ingredient.insertMany(ingredientObjects);
     res.status(200).json({ ingredients: insertedIngredients });
 })
